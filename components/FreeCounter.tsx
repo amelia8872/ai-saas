61432523984143d85ed980642714ea33ae8f3a49
amelia8872 <jkramelia8872@gmail.com>
2024-08-13 16:20:49 -0400

    add sidebar free generation bar

--- components/FreeCounter.tsx
+++ components/FreeCounter.tsx
@@ -2,6 +2,10 @@
 
 import { useState, useEffect } from 'react';
 import { Card, CardContent } from '@/components/ui/card';
+import { MAX_FREE_COUNTS } from '@/constants';
+import { Progress } from '@/components/ui/progress';
+import { Button } from '@/components/ui/button';
+import { Zap } from 'lucide-react';
 
 interface FreeCounterProps {
   apiLimitCount: number;
@@ -23,7 +27,17 @@ export const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
       <Card className="bg-white/10 border-0">
         <CardContent className="py-6">
           <div className="text-center text-sm text-white mb-4 space-y-2">
-            <p>{apiLimitCount}</p>
+            <p>
+              {apiLimitCount}/{MAX_FREE_COUNTS} Free Generations
+            </p>
+            <Progress
+              className="h-3"
+              value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
+            />
+            <Button className="w-full " variant="premium">
+              Upgrade
+              <Zap className="w-4 h-4 ml-2 fill-white" />
+            </Button>
           </div>
         </CardContent>
       </Card>
