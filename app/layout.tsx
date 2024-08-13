82f932cc7173c6f649295f9395e8ea6f49ab6fbf
amelia8872 <jkramelia8872@gmail.com>
2024-08-13 18:41:52 -0400

    front end pro modal render

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+import './globals.css';
+import { ModalProvider } from '@/components/Modal-provider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Genius",
-  description: "AI Platform",
+  title: 'Genius',
+  description: 'AI Platform',
 };
 
 export default function RootLayout({
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
