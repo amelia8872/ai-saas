a804615eb6166e3b040cde924e5549bd14ecd7a1
amelia8872 <jkramelia8872@gmail.com>
2024-08-19 01:44:36 -0400

    modify landing page export

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -5,7 +5,7 @@ import LandingLayout from './LandingLayout';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
-export const LandingPage = () => {
+const LandingPage = () => {
   return (
     <main className="min-h-screen bg-[#111827] overflow-auto">
       <div className="mx-auto max-w-screen-xl h-full w-full">
