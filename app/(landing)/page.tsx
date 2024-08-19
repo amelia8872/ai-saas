4c3180ab6197d8cd5059e98fb14c489af1975462
amelia8872 <jkramelia8872@gmail.com>
2024-08-19 01:37:03 -0400

    add testimonials

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,6 @@
 import { LandingNavbar } from '@/components/Landing-navbar';
 import { LandingHero } from '@/components/Landing-hero';
+import { LandingContent } from '@/components/Landing-content';
 import LandingLayout from './LandingLayout';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
@@ -10,6 +11,7 @@ export const LandingPage = () => {
       <div className="mx-auto max-w-screen-xl h-full w-full">
         <LandingNavbar />
         <LandingHero />
+        <LandingContent />
       </div>
     </main>
   );
